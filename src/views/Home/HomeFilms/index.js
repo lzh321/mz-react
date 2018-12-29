01fb0e32a1cc3136710029e1bb7c602c8f3a349e
LZH <1604807798@qq.com>
2018-12-29 11:55:23 +0800

    城市定位

--- src/views/Home/HomeFilms/index.js
+++ src/views/Home/HomeFilms/index.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Banner from '../../../components/Banner';
 import FilmsTabs from './FilmsTabs'
 import './index.scss';
+import { Localtion } from './style';
 class Films extends Component {
   render(){
-    return <div className="homefilms">
+    return (
+      <div className="homefilms">
+        <Localtion>
+          <span>深圳市</span>
+          <i className="iconfont icon-xialazhankai"></i>
+        </Localtion>
         <Banner />
         <FilmsTabs />
       </div>
+    )
   }
 }
 
---
+++ src/views/Home/HomeFilms/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+// 定义定位的组件
+
+export const Localtion = styled.div`
+position: absolute;
+top: 5px;
+left: 5px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size:13px;
+color:white;
+border-radius:15px;
+height:25px;
+width:65px;
+background:black;
+opacity:0.6;
+z-index:1;
+`
+
+export const Item = styled.i`
+font-size:15px;
+`;
